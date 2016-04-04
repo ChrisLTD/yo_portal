b9757d79cb9e16cb4ff5501f4f52f417d1a9d559
Chris Johnson <chrisltd@gmail.com>
2016-04-04 10:57:58 -0400

    removed skimmer link; reformatted CSS

--- _fetchfeeds.php
+++ _fetchfeeds.php
@@ -20,31 +20,31 @@ if(isset($_COOKIE['longitude'])){
 
 // Setup RSS Feeds
 
-$nyt = new SimplePie();  
-$nyt->set_feed_url('http://feeds.nytimes.com/nyt/rss/HomePage');  
-$nyt->enable_cache(true);  
-$nyt->set_cache_duration($portal_cache_duration);  
-$nyt->set_cache_location($portal_cache_location);  
+$nyt = new SimplePie();
+$nyt->set_feed_url('http://feeds.nytimes.com/nyt/rss/HomePage');
+$nyt->enable_cache(true);
+$nyt->set_cache_duration($portal_cache_duration);
+$nyt->set_cache_location($portal_cache_location);
 
-$markets = new SimplePie();  
-$markets->set_feed_url('http://pipes.yahoo.com/pipes/pipe.run?_id=ZKJobpaj3BGZOew9G8evXg&_render=rss&ticker=' . urlencode(implode(array_keys($portal_stock_symbols), ",")));  
-$markets->enable_cache(true);  
-$markets->set_cache_duration($portal_cache_duration);  
-$markets->set_cache_location($portal_cache_location);  
+$markets = new SimplePie();
+$markets->set_feed_url('http://pipes.yahoo.com/pipes/pipe.run?_id=ZKJobpaj3BGZOew9G8evXg&_render=rss&ticker=' . urlencode(implode(array_keys($portal_stock_symbols), ",")));
+$markets->enable_cache(true);
+$markets->set_cache_duration($portal_cache_duration);
+$markets->set_cache_location($portal_cache_location);
 
-$woot = new SimplePie();  
-$woot->set_feed_url('http://www.woot.com/salerss.aspx');  
-$woot->set_item_class("SimplePie_Item_Woot");  
-$woot->enable_cache(true);  
-$woot->set_cache_duration($portal_cache_duration);  
-$woot->set_cache_location($portal_cache_location);  
-  
-$wootshirt = new SimplePie();  
-$wootshirt->set_feed_url('http://shirt.woot.com/salerss.aspx');  
-$wootshirt->set_item_class("SimplePie_Item_Woot");  
-$wootshirt->enable_cache(true);  
-$wootshirt->set_cache_duration($portal_cache_duration);  
-$wootshirt->set_cache_location($portal_cache_location);  
+$woot = new SimplePie();
+$woot->set_feed_url('http://www.woot.com/salerss.aspx');
+$woot->set_item_class("SimplePie_Item_Woot");
+$woot->enable_cache(true);
+$woot->set_cache_duration($portal_cache_duration);
+$woot->set_cache_location($portal_cache_location);
+
+$wootshirt = new SimplePie();
+$wootshirt->set_feed_url('http://shirt.woot.com/salerss.aspx');
+$wootshirt->set_item_class("SimplePie_Item_Woot");
+$wootshirt->enable_cache(true);
+$wootshirt->set_cache_duration($portal_cache_duration);
+$wootshirt->set_cache_location($portal_cache_location);
 
 function getLocationName($latitude, $longitude, $username){
 	$request_url = 'http://api.geonames.org/findNearbyPlaceNameJSON?lat=' . $latitude . '&lng=' . $longitude . '&username=' . $username;
@@ -53,13 +53,13 @@ function getLocationName($latitude, $longitude, $username){
 		$location_object = json_decode($content);
 		return $location_object->geonames[0]->name;
 	} else {
-		return false;		
+		return false;
 	}
 }
 
 ?>
 
-<?php 
+<?php
   if($nyt->init()):
     $nyt->handle_content_type();
     $nytitems = $nyt->get_items();
@@ -67,11 +67,11 @@ function getLocationName($latitude, $longitude, $username){
     if( count($nytitems) > 0 ):
  ?>
 <div class="column">
-  <h2><a href="http://www.nytimes.com?_r=1&partner=rss&emc=rss">NYTimes</a> <em><a href="http://www.nytimes.com/skimmer">skimmer</a></em></h2>
+  <h2><a href="http://www.nytimes.com?_r=1&partner=rss&emc=rss">NYTimes</a></h2>
 
   <ul class="news">
-    <? 
-    foreach($nytitems as $item): 
+    <?
+    foreach($nytitems as $item):
      $description = trim(strip_tags($item->get_description()));
       ?>
       <li><a href="<?=$item->get_link()?>" title="<?=$description;?>"><?=$item->get_title(); ?></a></li>
@@ -79,32 +79,32 @@ function getLocationName($latitude, $longitude, $username){
   </ul>
 
 </div><!--/.column-->
-<?php 
-    endif; 
-  endif; 
+<?php
+    endif;
+  endif;
 ?>
 
 
 <div class="column">
 
-<?php 
+<?php
 	$cachefile = $portal_cache_location . "/weather" . $portal_latitude . $portal_longitude;
 	$cachetime = 20 * 60; // 20 minutes
-	
+
 	// Serve from the cache if it is younger than $cachetime
 	if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
 		include($cachefile);
 		echo "<!-- Cached ".date('jS F Y H:i', filemtime($cachefile))." -->";
 	} else {
 		ob_start(); // start the output buffer
-	
-		$forecast = new ForecastIO($portal_forecastio_api_key);	
-	
+
+		$forecast = new ForecastIO($portal_forecastio_api_key);
+
 		if($condition = $forecast->getCurrentConditions($portal_latitude, $portal_longitude)):
 ?>
 
   <h2>Weather</h2>
-  
+
   <ul class="weather">
     <li>
     	<canvas id="weather_icon" width="100" height="100" data-weather-state="<?php echo strtoupper($condition->getIcon());?>"></canvas>
@@ -119,9 +119,9 @@ function getLocationName($latitude, $longitude, $username){
     	<br><b><?php echo round($condition->getTemperature()); ?>&deg;</b> and <b><?php echo $condition->getSummary(); ?></b>
     </li>
   </ul>
-  
-<?php 
-		endif; 
+
+<?php
+		endif;
 
 		$fp = fopen($cachefile, 'w'); // open the cache file for writing
 		fwrite($fp, ob_get_contents()); // save the contents of output buffer to the file
@@ -132,21 +132,21 @@ function getLocationName($latitude, $longitude, $username){
 	} // end else
 ?>
 
-<?php 
+<?php
   if($markets->init()):
     $markets->handle_content_type();
     $marketitems = $markets->get_items();
     if( count($marketitems) > 0 ):
  ?>
-    
+
   <h2>Stocks</h2>
-  
+
   <table class="markets">
 
-  <? 
-  foreach($marketitems as $item) : 
-  ?>  
-    <? 
+  <?
+  foreach($marketitems as $item) :
+  ?>
+    <?
     $title = $item->get_title();
     $splittitle = explode(" ", $title);
     $symbol = $splittitle[0];
@@ -161,9 +161,9 @@ function getLocationName($latitude, $longitude, $username){
 			<td><?=$price;?></td>
 			<td>
 			<? if(strpos($pricechange, "-") === 0) //If the negative sign is the 1st char
-					{ echo '<span class="down">' . round($pricechange, 2) . '</span>';} 
+					{ echo '<span class="down">' . round($pricechange, 2) . '</span>';}
 				 else{
-						{ echo '<span class="up">' . round($pricechange, 2) . '</span>';} 
+						{ echo '<span class="up">' . round($pricechange, 2) . '</span>';}
 				 }
 			?>
 			</td>
@@ -171,18 +171,18 @@ function getLocationName($latitude, $longitude, $username){
   <? endforeach; ?>
   </table>
 
-<?php 
-    endif; 
-  endif; 
+<?php
+    endif;
+  endif;
 ?>
 
-<?php 
+<?php
   if($woot->init()):
     $woot->handle_content_type();
-?>   
+?>
 
   <h2>Woot</h2>
-  
+
   <ul class="woot">
     <li>
       <? $item = $woot->get_item(); ?>
@@ -197,17 +197,17 @@ function getLocationName($latitude, $longitude, $username){
         if($soldout=="true"): ?>
         	<em>Sold Out</em>
         <?php else: ?>
-        <?=$item->get_price();?> 
+        <?=$item->get_price();?>
         	<?php if($wootoff=="true"): ?>
         		<span class="info"><?=(1-$item->get_soldout_percentage()) * 100;?>% left</span>
-      		<?php endif; ?>  	
+      		<?php endif; ?>
 			<?php endif; ?>
     </li>
-    
-    <?php 
+
+    <?php
       if($wootshirt->init()):
         $wootshirt->handle_content_type();
-    ?>   
+    ?>
     <li>
       <? $item = $wootshirt->get_item(); ?>
       <a href="<?=$item->get_link();?>"><img src="<?=$item->get_standard_src();?>"><br>
